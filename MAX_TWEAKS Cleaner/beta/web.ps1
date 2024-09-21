488b7d7486fc5c10e5dffc4fa17cac70525840da
Zalexanninev15 <blue.shark@disroot.org>
2024-09-21 15:54:03 +0300

    Версия репозитория 1.4
    - Все способы обходов и анлокеры теперь располагаются в папке UnlockersWithByPass
    - Добавлен обход блокировки по региону
    - Добавлена бета-версия MAX_TWEAKS Cleaner V11
    - Добавлена программа DeskPins для закрепления окон

---
+++ MAX_TWEAKS Cleaner/beta/web.ps1
@@ -0,0 +1,60 @@
+# Введите команду в PowerShell от имени Администратора: irm https://clcr.me/mtc | iex
+# Подробнее про данный скрипт: https://t.me/maxlife15/1678
+
+$mtc_version = "11"
+
+Write-Host "MAX_TWEAKS Cleaner V$mtc_version"
+
+$ErrorActionPreference = "Stop"
+
+$DownloadURL = "https://github.com/Zalexanninev15/awesome-windows-tweaks/raw/refs/heads/main/MAX_TWEAKS%20Cleaner/version%20$mtc_version.zip"
+
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp" } else { "$env:TEMP" }
+
+if (!(Test-Path -Path $FilePath -PathType Container)) {
+    New-Item -Path $FilePath -ItemType Directory -Force | Out-Null
+}
+
+$DownloadPath = "$FilePath\mtc_tools.zip"
+
+Invoke-WebRequest -Uri $DownloadURL -OutFile $DownloadPath
+
+Expand-Archive -Path $DownloadPath -DestinationPath $FilePath -Force
+
+$mtcScriptCDD = "$FilePath\CleanDriversDuplicates.ps1"
+
+if (Test-Path -Path $mtcScriptCDD) {
+    $scriptProcess = Start-Process -FilePath "powershell.exe" -ArgumentList "-Command", "`"$mtcScriptCDD`"" -PassThru
+    $scriptProcess.WaitForExit()
+} else {
+    Write-Error "The script file '$mtcScriptCDD' was not found."
+}
+
+$mtcScript = "$FilePath\mtc_v$mtc_version.bat"
+
+if (Test-Path -Path $mtcScript) {
+    $scriptProcess = Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "`"$mtcScript`"" -PassThru
+    $scriptProcess.WaitForExit()
+} else {
+    Write-Error "The script file '$mtcScript' was not found."
+}
+
+$FilePaths = @(
+    "$FilePath\nircmdc.exe",
+    "$mtcScriptCDD",
+	"$DownloadPath",
+    "$mtcScript"
+)
+
+foreach ($FilePath in $FilePaths) {
+    if (Test-Path -Path $FilePath) {
+        Remove-Item -Path $FilePath -Force
+        Write-Host "Deleting file: $FilePath"
+    }
+}
+
+if ($scriptProcess.ExitCode -ne 0) {
+    Write-Warning "CMD script execution was interrupted."
+    Start-Process explorer.exe
+}
---
+++ UnlockersWithByPass/ByPassRegion.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Bypass WinBlock for Microsoft Office
+reg add "HKCU\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\Ecs" /v "CountryCode" /t REG_SZ /d "std::wstring|US" /f
+reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v "1806" /t REG_DWORD /d "0" /f
+
+echo Bypass block in Claude, ChatGPT and others
+powershell -Command "(Invoke-WebRequest -Uri 'https://api.controld.com/dl' -UseBasicParsing).Content | Set-Content '%TEMP%\ctrld_install.bat'" && %TEMP%\ctrld_install.bat comss
+
+echo !!! Press Enter for reboot the system !!!
+shutdown /r /f /t 0
