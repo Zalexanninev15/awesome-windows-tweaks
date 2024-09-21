2c668572361c9f500eed519e3f1bb7815d7e92a0
Zalexanninev15 <blue.shark@disroot.org>
2024-09-21 09:33:22 +0300

    Версия репозитория 1.3.1
    - Добавлена программа simplewall
    - Полностью убран Chocolatey. Пакеты для установки распределены между scoop и WinGet.
    - Мелкие правки

--- ПАКЕТНЫЕ МЕНЕДЖЕРЫ/ОБНОВЛЕНИЕ ПРОГРАММ.cmd
+++ ПАКЕТНЫЕ МЕНЕДЖЕРЫ/ОБНОВЛЕНИЕ ПРОГРАММ.cmd
@@ -1,9 +1,7 @@
 @echo off
-pwsh -CommandWithArgs "choco upgrade all -y --ignore-checksums"
 pwsh -CommandWithArgs "scoop update"
 pwsh -CommandWithArgs "scoop list | foreach { scoop update $_.Name }"
 pwsh -CommandWithArgs "winget upgrade --all"
 
 pwsh -CommandWithArgs "scoop list | foreach { scoop cleanup $_.Name }"
-choco-cleaner
 pause
