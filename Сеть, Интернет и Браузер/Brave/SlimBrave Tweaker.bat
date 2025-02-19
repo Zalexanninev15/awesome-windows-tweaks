14b457db002bfe24ccaeee430d1480df9847ed1c
Max <blue.shark@disroot.org>
2025-02-20 00:28:54 +0300

    Твикер SlimBrave для браузера Brave

---
+++ Сеть, Интернет и Браузер/Brave/SlimBrave Tweaker.bat
@@ -0,0 +1,11 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "if (-Not ([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { exit 1 } else { exit 0 }"
+
+if %errorlevel% equ 1 (
+    echo Run as Administrator!
+    pause
+	exit
+)
+
+powershell -c "irm https://clcr.me/slimbrave | iex"
+exit /b
